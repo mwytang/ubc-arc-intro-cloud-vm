0033dd61b3f7635d054ad5e43d1bcb714ebe42e0
Michael Tang <michael.tang@ubc.ca>
2024-06-27 14:54:00 -0700

    Update conf.py - documentation title

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'Intro to Cloud and VM'
+copyright = '2024, M Tang'
+author = 'M Tang'
 
 release = '0.1'
 version = '0.1.0'
